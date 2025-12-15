b3a5895022c09aa9493db3063a069db3aff48981
Yoran Cobben <yorancobben@Yorans-MacBook-Pro.local>
2025-12-15 13:06:50 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/DressMyRide/sw.js', { scope: '/DressMyRide/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/VeloKit/sw.js', { scope: '/VeloKit/' })})}
